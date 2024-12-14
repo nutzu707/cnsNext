c5c30b5014067ff6de9f3b419506f3e720745114
nutzu707 <nutzusandor@gmail.com>
2024-12-14 20:25:10 +0200

    commit

---
+++ src/app/(pages)/management-documente/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/navbar/page";
+
+export default function ManagementDocumente() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Management Documente</h1>
+        </div>
+    );
+}
